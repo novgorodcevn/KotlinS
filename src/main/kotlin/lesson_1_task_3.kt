e12863afb43737411e9ddaf8ac968080c9df822d
Nikita Novgorodtsev <novgorodcevn5@gmail.com>
2025-08-31 16:07:55 +0300

    KS-1-3 добавил и проинициализировал 3 переменные полета Гагарина в космос

---
+++ src/main/kotlin/lesson_1_task_3.kt
@@ -0,0 +1,14 @@
+ fun main(){
+     val theYearOfYuriGagarin = 1961
+     var hour= "09"
+     var minute= "07"
+     println(theYearOfYuriGagarin)
+     println(hour)
+     println(minute)
+
+     hour="10"
+     minute="55"
+     print(hour)
+     print(":")
+     print(minute)
+ }
