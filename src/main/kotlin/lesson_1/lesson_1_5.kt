35183d075b4a3ae083d52f5e497b044fbce1810b
Nikita Novgorodtsev <novgorodcevn5@gmail.com>
2025-09-03 23:59:53 +0300

    KS-1-5 убрал функцию,добавил константы

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,14 +1,18 @@
 package lesson_1
 
+const val SECONDS_IN_AN_HOUR = 3600
+const val SECONDS_IN_A_MINUTE = 60
 fun main() {
-    var seconds: Short = 6480
-    timeSpentInSpace(seconds)
-
+    val timeInSpace: Short = 6480
+    var time = timeInSpace / SECONDS_IN_AN_HOUR
+    var minutes = timeInSpace % SECONDS_IN_AN_HOUR / SECONDS_IN_A_MINUTE
+    var seconds = timeInSpace % SECONDS_IN_AN_HOUR % SECONDS_IN_A_MINUTE
+    var resultTime = String.format("%02d:%02d:%02d", time, minutes, seconds)
+    println(resultTime)
 }
-   fun timeSpentInSpace(seconds:Short):String{
-       var time = seconds/3600
-       var minutes = seconds%3600/60
-       var seconds = seconds%3600%60
-       var resultTime = String.format("%02d:%02d:%02d",time,minutes,seconds)
-       return println(resultTime).toString()
-   }
+
+
+
+
+
+
