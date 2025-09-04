6263b51ebcebf27602b45f5d49eaadf71ca2cd08
Nikita Novgorodtsev <novgorodcevn5@gmail.com>
2025-09-04 17:12:09 +0300

    KS-2-3 исправил все отступы,формат

--- src/main/kotlin/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2_task_3.kt
@@ -1,11 +1,12 @@
-fun main(){
-      val departureHour = 9
-      val departureMinute = 39
-      val travelTimeMinute = 457
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTimeMinute = 457
 
-      val arrivalInMinutes = departureHour*MINUTE_IN_A_HOUR+departureMinute+travelTimeMinute
-      val hourFormat = arrivalInMinutes/MINUTE_IN_A_HOUR
-      val minuteFormat = arrivalInMinutes%MINUTE_IN_A_HOUR
-      println("$hourFormat:$minuteFormat")
+    val arrivalInMinutes = departureHour * MINUTE_IN_A_HOUR + departureMinute + travelTimeMinute
+    val hourFormat = arrivalInMinutes / MINUTE_IN_A_HOUR
+    val minuteFormat = arrivalInMinutes % MINUTE_IN_A_HOUR
+    println(String.format("%02d:%02d", hourFormat, minuteFormat))
 }
+
 const val MINUTE_IN_A_HOUR = 60
