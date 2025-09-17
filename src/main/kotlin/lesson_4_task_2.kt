a326dd32ab0047cf75014f4d701c5c66b87e903a
Nikita Novgortcev <n42730689@gmail.com>
2025-09-17 11:29:44 +0300

    Сравнение груза: Average

---
+++ src/main/kotlin/lesson_4_task_2.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var weight = 20
+    var volume = 80
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            weight > MIN_WEIGHT && weight <= MAX_WEIGHT
+                    && volume < MAX_LITER
+        }"
+    )
+    weight = 50
+    volume = 100
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            weight > MIN_WEIGHT && weight <= MAX_WEIGHT
+                    && volume < MAX_LITER
+        }"
+    )
+}
+
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_LITER = 100
