d940e42093d6e05ffd4a08f4007730162ce937d0
Nikita Novgortcev <n42730689@gmail.com>
2025-09-18 19:54:38 +0300

    KS-4-5-сделал программу c входными данными

---
+++ src/main/kotlin/lesson_4_task_5.kt
@@ -0,0 +1,22 @@
+fun main() {
+    print("Наличие повреждений корпуса (true/false): ")
+    val damage = readln().toBoolean()
+    print("Текущий состав экипажа: ")
+    val countCrew = readln().toInt()
+    print("Количество ящиков с провизией на борту: ")
+    val boxProvisions = readln().toInt()
+    print("Благоприятность метеоусловий (true/false): ")
+    val weather = readln().toBoolean()
+
+    val settingSail =
+        (!damage && countCrew in MIN_CREW..MAX_CREW && boxProvisions > MAX_PROVISIONS && (weather == FAVORABLE || weather == UNFAVORABLE))
+                || (damage && countCrew == MAX_CREW && weather == FAVORABLE && boxProvisions >= MAX_PROVISIONS)
+    println(settingSail)
+
+}
+
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val MAX_PROVISIONS = 50
+const val FAVORABLE = true
+const val UNFAVORABLE = false
