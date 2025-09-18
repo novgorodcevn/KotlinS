9ba7ef2eeae2017c376b60f9eb0bb588fc624d99
Nikita Novgortcev <n42730689@gmail.com>
2025-09-18 10:50:03 +0300

    KS-3-5 шахматы,парс строки с сервера

---
+++ src/main/kotlin/lesson_3_task_5.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val blackMove = "D2-D4;0"
+
+    val whereFrom = "${blackMove[0]}${blackMove[1]}"
+    val whereTo = "${blackMove[3]}${blackMove[4]}"
+    val moveNumber = "${blackMove[6]}"
+    println(whereFrom)
+    println(whereTo)
+    println(moveNumber)
+}
