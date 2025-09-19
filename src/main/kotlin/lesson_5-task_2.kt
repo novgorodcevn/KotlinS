b37e76a9c4990a12513b49bdad7c146aff730293
Nikita Novgortcev <n42730689@gmail.com>
2025-09-20 01:52:58 +0300

    KS-5-2-применил преременную с годом рождения

---
+++ src/main/kotlin/lesson_5-task_2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val userYear = readln().toInt()
+    val userAge = CURRENT_YEAR - userYear
+    if (userAge >= AGE_OF_MAJORITY)
+        println("Показать экран со скрытым контентом")
+}
+
+const val AGE_OF_MAJORITY = 18
+const val CURRENT_YEAR = 2025
