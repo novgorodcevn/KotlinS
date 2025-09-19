20f52a1ae9d52db77cd6858e0d3586a76b7b0bc3
Nikita Novgortcev <n42730689@gmail.com>
2025-09-19 13:15:03 +0300

    KS-3-5-применил функцию split()

--- src/main/kotlin/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3_task_5.kt
@@ -1,9 +1,9 @@
 fun main() {
-    val blackMove = "D2-D4;0"
+    val blackMove = "D2-D4;0".split("-", ";")
 
-    val whereFrom = "${blackMove[0]}${blackMove[1]}"
-    val whereTo = "${blackMove[3]}${blackMove[4]}"
-    val moveNumber = "${blackMove[6]}"
+    val whereFrom = blackMove[0]
+    val whereTo = blackMove[1]
+    val moveNumber = blackMove[2]
     println(whereFrom)
     println(whereTo)
     println(moveNumber)
