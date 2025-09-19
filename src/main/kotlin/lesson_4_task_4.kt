028dad98c8c43de539bd2f21ac288ed4427a0d51
Nikita Novgortcev <n42730689@gmail.com>
2025-09-19 13:26:30 +0300

    KS-4-4-вынес результат в переменные

--- src/main/kotlin/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4_task_4.kt
@@ -1,11 +1,13 @@
 fun main() {
     val isToday = 1
+    val evenDay = (isToday % 2 == 0)
+    val oddDay = (isToday % 2 !== 0)
 
     val muscleGroups = """
-        Упражнения для рук:    ${isToday % 2 !== 0}
-        Упражнения для ног:    ${isToday % 2 == 0}
-        Упражнения для спины:  ${isToday % 2 == 0}
-        Упражнения для пресса: ${isToday % 2 !== 0}
+        Упражнения для рук:    $oddDay
+        Упражнения для ног:    $evenDay
+        Упражнения для спины:  $evenDay
+        Упражнения для пресса: $oddDay
     """.trimIndent()
     println(muscleGroups)
 }
