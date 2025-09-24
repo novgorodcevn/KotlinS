ec03146077586e83cfff0a0e264a58722ad2e194
Nikita Novgortcev <n42730689@gmail.com>
2025-09-24 16:26:22 +0300

    KS-5-4-исправил посд=ледовательность действий

--- src/main/kotlin/lesson_5_task_4.kt
+++ src/main/kotlin/lesson_5_task_4.kt
@@ -10,28 +10,28 @@ fun main() {
     Да, вперед, пожалуйста, вводите свои данные...
     """.trimIndent()
     println(marvinGreeting)
+
     val userName = readln()
-    val userPassword = readln()
     if (userName == USER_NAME) {
-        if (userPassword == USER_PASSWORD) {
-            val resultMarvin = """
+        print("Пароль:")
+    } else {
+        print("Вам нужно зарегестрироваться")
+        return
+    }
+    val userPassword = readln()
+    if (userPassword == USER_PASSWORD) {
+        val resultMarvin = """
                [вздыхает...] Ваши данные проверены, и о, чудо, они верны...
                Пользователь "Zaphod", вам разрешено входить на борт корабля 
                "Heart of Gold". Хотя мне всё равно... Ну вперед, войдите... 
                Если вам так уж надо, в конце концов... [меланхолический вздох...] 
                Надеюсь, вам понравится пребывание здесь больше, чем мне.
             """.trimIndent()
-            println(resultMarvin)
-        } else {
-            println("Неверный пароль")
-            return
-        }
+        println(resultMarvin)
     } else {
-        println("Вам нужно зарегестрироваться")
-        return
+        println("Неверный пароль")
     }
 }
 
 const val USER_NAME = "Zaphod"
 const val USER_PASSWORD = "PanGalactic"
-
