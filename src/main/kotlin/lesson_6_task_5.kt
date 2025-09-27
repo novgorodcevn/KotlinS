3e9c5b8e338ccd32c53faa2850b3ae84fabc6a1f
Nikita Novgortcev <n42730689@gmail.com>
2025-09-27 17:00:19 +0300

    KS-6-5-исправил условие if (userAnswer == null)

--- src/main/kotlin/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6_task_5.kt
@@ -1,30 +1,33 @@
 fun main() {
-
     println("Докажите что вы не бот")
     var attempt = 3
 
     do {
-        attempt--
-        val firstNumber = (1..9).random()
-        val secondNumber = (1..9).random()
-        val resultSum = firstNumber + secondNumber
+        var firstNumber = (1..9).random()
+        var secondNumber = (1..9).random()
+        var resultSum = firstNumber + secondNumber
         print("Сложите два числа:$firstNumber+$secondNumber=")
-        val userAnswer = readln().toIntOrNull()
+        var userAnswer = readln().toIntOrNull()
         if (userAnswer == null) {
             println("Вы ввели не число")
-            return
+            println("Попробуйте еще раз:")
+            firstNumber = (1..9).random()
+            secondNumber = (1..9).random()
+            resultSum = firstNumber + secondNumber
+            print("Сложите два числа:$firstNumber+$secondNumber=")
+            userAnswer = readln().toIntOrNull()
         }
         when {
-            resultSum != userAnswer -> println("Предлагаю решить его заново")
+            resultSum != userAnswer -> {
+                attempt--
+                println("Предлагаю решить его заново")
+            }
             resultSum == userAnswer -> {
                 println("Добро пожаловать!")
                 return
             }
         }
-
     } while (attempt > 0)
 
-    val resultMessage = "Доступ запрещен."
-
-    println(resultMessage)
+    println("Доступ запрещен.")
 }
