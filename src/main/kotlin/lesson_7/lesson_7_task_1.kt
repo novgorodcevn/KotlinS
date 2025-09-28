c16e9f5f64f8d17814bb84fa13643cfe85bc1e54
Nikita Novgortcev <n42730689@gmail.com>
2025-09-28 12:08:04 +0300

    KS-7-1-определил переменные перед циклом

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -2,15 +2,13 @@ package lesson_7
 
 fun main() {
     val passwordGeneration = mutableListOf<String>()
-
-    for (i in 1..NUMBER_OF_CHARACTERS) {
-        val symbol = ('a'..'z').random().toString()
-        val number = (0..9).random().toString()
-        passwordGeneration.add(symbol)
-        passwordGeneration.add(number)
+    val symbol = ('a'..'z')
+    val number = (0..9)
+    for (i in 1..NUMBER_OF_CHARACTERS_PAIRS) {
+        passwordGeneration.add(symbol.random().toString())
+        passwordGeneration.add(number.random().toString())
     }
-    val singlePassword = listOf(passwordGeneration.joinToString (""))
-    println(singlePassword)
+    println(passwordGeneration.joinToString (""))
 }
 
-const val NUMBER_OF_CHARACTERS = 3
+const val NUMBER_OF_CHARACTERS_PAIRS = 3
