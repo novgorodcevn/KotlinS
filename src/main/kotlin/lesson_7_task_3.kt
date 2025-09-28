bb437d619cbd359036d8512213aa88cc23d19825
Nikita Novgortcev <n42730689@gmail.com>
2025-09-28 16:14:02 +0300

    KS-7-3-убрал избыточную переменную,добавил скобки

--- src/main/kotlin/lesson_7_task_3.kt
+++ src/main/kotlin/lesson_7_task_3.kt
@@ -1,9 +1,9 @@
 fun main() {
     print("Введите число:")
     val userAnswer = readln().toInt()
-    val userNumber = userAnswer
-    val evenRange = 0..userNumber step 2
+    val evenRange = 0..userAnswer step 2
 
-    for (i in evenRange)
+    for (i in evenRange) {
         println(i)
+    }
 }
