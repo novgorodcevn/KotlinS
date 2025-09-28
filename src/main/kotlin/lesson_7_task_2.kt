dfbfbaf82bf49f3c05e6ab1133b78bf356d8693f
Nikita Novgortcev <n42730689@gmail.com>
2025-09-28 16:10:07 +0300

    KS-7-5-убрал избыточный оператор,добавил пробелы

--- src/main/kotlin/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7_task_2.kt
@@ -5,8 +5,7 @@ fun main() {
         println("Ваш код авторизации:${smsCode}")
         print("Введите ваш код для авторизации:")
         val userCode = readln().toInt()
-        if (smsCode==userCode){
-            println("Приветсвую вас")
-        }
-    }while (smsCode!=userCode)
+    }
+    while (smsCode != userCode)
+    println("Приветсвую вас")
 }
