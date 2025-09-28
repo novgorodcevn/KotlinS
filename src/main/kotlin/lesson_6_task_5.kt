7688b8420aa1fde0d5667be5a24d39d0022853ef
Nikita Novgortcev <n42730689@gmail.com>
2025-09-28 10:36:21 +0300

    KS-6-5-применил условие if elso,добавил continue

--- src/main/kotlin/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6_task_5.kt
@@ -3,29 +3,22 @@ fun main() {
     var attempt = 3
 
     do {
-        var firstNumber = (1..9).random()
-        var secondNumber = (1..9).random()
-        var resultSum = firstNumber + secondNumber
+        val firstNumber = (1..9).random()
+        val secondNumber = (1..9).random()
+        val resultSum = firstNumber + secondNumber
         print("Сложите два числа:$firstNumber+$secondNumber=")
-        var userAnswer = readln().toIntOrNull()
+        val userAnswer = readln().toIntOrNull()
         if (userAnswer == null) {
             println("Вы ввели не число")
             println("Попробуйте еще раз:")
-            firstNumber = (1..9).random()
-            secondNumber = (1..9).random()
-            resultSum = firstNumber + secondNumber
-            print("Сложите два числа:$firstNumber+$secondNumber=")
-            userAnswer = readln().toIntOrNull()
+            continue
         }
-        when {
-            resultSum != userAnswer -> {
-                attempt--
-                println("Предлагаю решить его заново")
-            }
-            resultSum == userAnswer -> {
-                println("Добро пожаловать!")
-                return
-            }
+        if (resultSum != userAnswer) {
+            attempt--
+            println("Предлагаю решить его заново")
+        } else {
+            println("Добро пожаловать!")
+            return
         }
     } while (attempt > 0)
 
