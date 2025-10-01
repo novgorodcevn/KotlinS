052b06fc5c52d589f32f6c72a0225806665d7a18
Nikita Novgortcev <n42730689@gmail.com>
2025-10-01 19:32:01 +0300

    KS-8-1-применил цикл forEach

---
+++ src/main/kotlin/lesson_9_task_1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val ingredientsDish = listOf("помидор", "сыр", "лук", "честнок", "морковь")
+    println("В рецепте еть следующие ингредиенты:")
+    ingredientsDish.forEach {
+        println(it)
+    }
+}
