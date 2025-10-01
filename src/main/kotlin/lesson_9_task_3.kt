e1a502f88e7a13ed70886102bbf47e4c3737c006
Nikita Novgortcev <n42730689@gmail.com>
2025-10-01 23:32:59 +0300

    KS-9-3-подсчитал весь результат в отдельном списке

---
+++ src/main/kotlin/lesson_9_task_3.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val amletProducts = listOf("яиц", "молока", "сливочного масла")
+    val quantityProducts = listOf(2, 50, 15)
+    val sizeProducts = listOf("шт", "мл", "гр")
+    val overallResultProducts = mutableListOf<String>()
+    println("Сколько хотите порций?")
+    val userPortions = readln().toInt()
+
+    val updatedQuantityProducts = quantityProducts.map {
+        it * userPortions
+    }
+
+    for (i in amletProducts.indices) {
+        val name = amletProducts[i]
+        val quantity = updatedQuantityProducts[i]
+        val size = sizeProducts[i]
+        val overallResult = "$name-$quantity$size"
+        overallResultProducts.add(overallResult)
+    }
+   println("На $userPortions порций вам понадобиться:${overallResultProducts.joinToString (",")}")
+}
