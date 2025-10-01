7162ed4fdc49041fc313fc4c6f46a4d945703e67
Nikita Novgortcev <n42730689@gmail.com>
2025-10-01 14:08:21 +0300

    KS-7-5-убрал условие when

--- src/main/kotlin/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7_task_5.kt
@@ -7,31 +7,24 @@ fun main() {
     do {
         print("Для генерации пароля введите введите количесво символов(минимальная длинна $MINIMUM_PASSWORD_LENGTH):")
         val userPasswordLength = readln().toIntOrNull()
+        val checkPasswordLength = userPasswordLength == null
         if (userPasswordLength == null || userPasswordLength < MINIMUM_PASSWORD_LENGTH) {
             println("Некоректное значение,попробуйте еще раз")
             continue
         } else {
-            generatedPassword.add(rangeNumbers.random().toChar())
+            generatedPassword.add(rangeNumbers.random())
             generatedPassword.add(rangeLetters.random())
             generatedPassword.add(rangeUppercaseLetters.random())
             for (i in 1..userPasswordLength - 3) {
-                val randomSymbol = (1..3).random()
-                when (randomSymbol) {
-                    1 -> {
-                        generatedPassword.add(rangeNumbers.random().toChar())
-                    }
-                    2 -> {
-                        generatedPassword.add(rangeLetters.random())
-                    }
-                    3 -> {
-                        generatedPassword.add(rangeUppercaseLetters.random())
-                    }
-                }
+                val unifiedSymbols =
+                    mutableListOf(rangeNumbers.random(), rangeLetters.random(), rangeUppercaseLetters.random())
+                val randomSymbol = (unifiedSymbols).random()
+                generatedPassword.add(randomSymbol)
             }
-            generatedPassword.shuffle()
-            println(generatedPassword.joinToString(""))
         }
-    } while (userPasswordLength == null || userPasswordLength < MINIMUM_PASSWORD_LENGTH)
+        generatedPassword.shuffle()
+        println(generatedPassword.joinToString(""))
+    } while (checkPasswordLength)
 }
 
 const val MINIMUM_PASSWORD_LENGTH = 6
