1b15d04aeddbe1e1bff41be4b8700a81a0112ade
Nikita Novgortcev <n42730689@gmail.com>
2025-10-01 20:47:09 +0300

    KS-9-2-добавил новый элемент

---
+++ src/main/kotlin/lesson_9_task_2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val ingredientsDish = mutableListOf("помидор", "сыр", "лук")
+    println("В рецепте есть базовые ингредиенты:$ingredientsDish")
+    println("Желаете добавить еще?")
+    val userAnswer = readln()
+    if (userAnswer.equals(POSITIVE_RESPONSE, true)) {
+        println("Каокой ингредиент вы хотите добавить?")
+        val userIngredient = readln()
+        ingredientsDish.add(userIngredient)
+        println("Теперь в рецепте есть следующие ингредиенты:$ingredientsDish")
+    } else {
+        return
+    }
+}
+
+const val POSITIVE_RESPONSE = "да"
