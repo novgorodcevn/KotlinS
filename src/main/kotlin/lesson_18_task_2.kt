e40792ed38b7f3313d43be839cc3d9d5d7c66d68
Nikita Novgortcev <n42730689@gmail.com>
2025-11-09 15:12:47 +0300

    KS-18-2 использовал реализацию в базовом классе

--- src/main/kotlin/lesson_18_task_2.kt
+++ src/main/kotlin/lesson_18_task_2.kt
@@ -1,31 +1,23 @@
-open class Dice() {
-
-    open fun rollTheDice() = ""
+open class Dice(
+    private val sides: Int,
+) {
+    fun rollTheDice(): String {
+        return "кубик брошен,число выпало:${(0..sides).random()}"
+    }
 }
 
 class Tetrahedron(
-    private val sides: Int = 4
-) : Dice() {
-    override fun rollTheDice(): String {
-        return "кубик брошен,число выпало:${(0..sides).random()}"
-    }
-}
+    sides: Int = 4
+) : Dice(sides)
 
 class Hexagon(
-    private val sides: Int = 6
-) : Dice() {
-    override fun rollTheDice(): String {
-        return "кубик брошен,число выпало:${(0..sides).random()}"
-    }
-}
+    sides: Int = 6
+) : Dice(sides)
 
 class Octahedron(
-    private val sides: Int = 8
-) : Dice() {
-    override fun rollTheDice(): String {
-        return "кубик брошен,число выпало:${(0..sides).random()}"
-    }
-}
+    sides: Int = 8
+) : Dice(sides)
+
 
 fun main() {
     val tetrahedron: Dice = Tetrahedron()
