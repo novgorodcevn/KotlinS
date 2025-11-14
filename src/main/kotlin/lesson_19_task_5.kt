0c0239d7b3a8b8cec897c2b0c4c7f543e7c145e9
Nikita Novgortcev <n42730689@gmail.com>
2025-11-15 00:20:33 +0300

    KS-19-5-применил companion object в enum()

---
+++ src/main/kotlin/lesson_19_task_5.kt
@@ -0,0 +1,39 @@
+enum class Gender(val displayName: String) {
+    MALE("Мужской"),
+    FEMALE("Женский");
+
+    companion object {
+        fun takeGender(gender: String): Gender? {
+            return Gender.entries.find { it.displayName.equals(gender, ignoreCase = true) }
+        }
+    }
+}
+
+class Person(val name: String, val gender: Gender)
+
+fun main() {
+    val listPerson = mutableListOf<Person>()
+
+    while (listPerson.size < 5) {
+        println("Введите имя")
+        val userName = readln()
+        if (userName == null) {
+            println("Вы ничего не ввели,попробуйте еще раз")
+            continue
+        }
+        println("Введите пол:мужской/женский")
+        val userGender = readln()
+        val gender = Gender.takeGender(userGender)
+        if (gender != null) {
+            listPerson.add(Person(name = userName, gender = gender))
+        } else {
+            println("Полл указан неверно")
+            println("Попробуйте еще раз")
+            continue
+        }
+    }
+    println("Список людей:")
+    listPerson.forEach {
+        println("Имя ${it.name} пол ${it.gender.displayName}")
+    }
+}
