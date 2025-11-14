5fe7c6ab860d888255389fbdfe5a61581d74eadb
Nikita Novgortcev <n42730689@gmail.com>
2025-11-14 19:02:09 +0300

    KS-19-3-описал методы падения программы

---
+++ src/main/kotlin/lesson_19_task_3.kt
@@ -0,0 +1,14 @@
+class Spaceship {
+
+    fun takeOff() {
+        TODO("Здесь нужна дополнительная логика взлёта космического корабля")
+    }
+
+    fun land() {
+        throw NotImplementedError("Метод приземления пока не реализован")
+    }
+
+    fun shoot() {
+        println("Выстрел произведён в астероид.")
+    }
+}
